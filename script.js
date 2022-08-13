1b0bd0dd5a0ce9a403eb958a269465e6640c8791
HarmonyP <harmonypeter91@gmail.com>
2022-08-13 19:04:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const contextMenu= document.querySelector('.wrapper'),
+shareMenu = document.querySelector('.share-item')
+
+document.addEventListener('contextmenu',(e)=>{
+  e.preventDefault()
+ 
+  let x = e.offsetX
+  let y = e.offsetY
+  let winWidth = window.innerWidth
+  let cmWidth= contextMenu.offsetWidth
+  let winHeight = window.innerHeight
+  let cmHeight= contextMenu.offsetHeight
+
+  if(x > (winWidth - cmWidth - shareMenu.offsetWidth)){
+    shareMenu.style.left='-200px'
+  }else{
+    shareMenu.style.left=''
+    shareMenu.style.right='-200px'
+  }
+
+
+  x= x>winWidth - cmWidth ? winWidth- cmWidth:x
+  y= y>winHeight - cmHeight ? winHeight- cmHeight:y
+  contextMenu.style.left=`${x}px`
+  contextMenu.style.top=`${y}px`
+  contextMenu.style.visibility="visible"
+})
+
+document.addEventListener('click',()=>{
+    contextMenu.style.visibility=''
+})
